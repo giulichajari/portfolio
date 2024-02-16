2639a3e88b14892d0bc1416d50b5608438a71789
EL CARU Caruso <giuli956@hotmail.com>
2024-02-16 17:50:28 -0300

    dd

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import '../global.css'
+export async function getServerSideProps() {
+  // Aquí puedes manejar la lógica para mostrar una página de error personalizada para el error 404
+  return {
+    props: {}, // No es necesario devolver ninguna prop
+  }
+}
+
+export default function Error() {
+  return <h1>Esta página no se encuentra</h1>
+}
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
