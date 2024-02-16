16c84c38a072bd406a11a6bab905f08eaa4354ef
EL CARU Caruso <giuli956@hotmail.com>
2024-02-16 17:46:02 -0300

    dd

---
+++ pages/404.page.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+const error404 = () => {
+    return (
+        <Layout>
+            <h1>BLOGhello world</h1>
+        </Layout>);
+};
+
+export default error404;
---
+++ pages/500.page.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+const error500 = () => {
+    return (
+        <Layout>
+            <h1>BLOGhello world</h1>
+        </Layout>);
+};
+
+export default error500;
