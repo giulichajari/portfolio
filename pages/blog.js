5640d4497362815d86ba290b9114bccbf6171639
EL CARU Caruso <giuli956@hotmail.com>
2024-02-15 22:22:48 -0300

    p

--- pages/blog.js
+++ pages/blog.js
@@ -1,15 +1,9 @@
 import Layout from "../components/Layout";
-export async function getServerSideProps() {
-    // Fetch data from external API
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const repo = await res.json()
-    // Pass data to the page via props
-    return { props: { repo } }
-  }
+
 const Blog = () => (
     <Layout>
         <h1>BLOGhello world</h1>
     </Layout>
 )
 
-export default Blog;
+export {Blog};
