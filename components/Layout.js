f5e0ce7e8b0c058f60f4853d45cabce7a261c44c
EL CARU Caruso <giuli956@hotmail.com>
2024-02-16 18:13:50 -0300

    dd

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,62 @@
-import NavBar from "./Navbar";
-import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+import PropTypes from "prop-types";
+import NProgress from "nprogress";
+import nProgress from "nprogress";
+import classNames from "classnames";
 
-const Layout = ({children}) => (
-    <>
-    <Head>
-        <title>Caruso - Mi Portfolio</title></Head>
-    <NavBar />
-    <main className="containter py-4">
-    {children}
-    </main> 
-    </>
-)
+const Layout = ({ children, title, footer = true, dark = false }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(url);
+      NProgress.start();
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    router.events.on("routeChangeComplete", () => NProgress.done());
+
+    router.events.on("routeChangeError", () => nProgress.done());
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <div className={classNames({ "bg-dark": dark, "bg-light": !dark })}>
+      <Navbar />
+      <main className="container py-4">
+        {/* Title */}
+        {title && (
+          <h1 className={classNames("text-center", { "text-light": dark })}>
+            {title}
+          </h1>
+        )}
+
+        {/* Content */}
+        {children}
+      </main>
+
+      {footer && (
+        <footer className="bg-dark text-light text-center">
+          <div className="container p-4">
+            <h1>&copy; Caruso - Mi Portfolio</h1>
+       
+          </div>
+        </footer>
+      )}
+    </div>
+  );
+};
+
+Layout.proptypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+  footer: PropTypes.bool,
+};
+
 export default Layout;
