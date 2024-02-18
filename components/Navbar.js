7b6f2123a2ea29dab171e283facb1ff174a6637f
EL CARU Caruso <giuli956@hotmail.com>
2024-02-18 17:37:46 -0300

    dd

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 const NavBar = () => {  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-            <Link legacyBehavior href="">
+            <Link href="/">
                 <a className="navbar-brand" >Mi Portfolio</a>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,12 +12,12 @@ const NavBar = () => {  return (
                 <ul className="navbar-nav ml-auto">
 
                     <li className="nav-item">
-                        <Link legacyBehavior href="">
+                        <Link href="">
                             <a className="nav-link">Blog</a>
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link legacyBehavior href="">
+                        <Link href="">
                             <a className="nav-link">GitHub</a>
                         </Link>
                     </li>
